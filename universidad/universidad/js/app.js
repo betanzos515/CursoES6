0a45a68180df163cd23737c11baa2ec4139c4c6c
betanzos515 <betanzos515@gmail.com>
2019-01-09 20:51:11 -0600

    Cuarta

--- universidad/universidad/js/app.js
+++ universidad/universidad/js/app.js
@@ -1,2 +1,19 @@
 // Eliminar de Local Storage
 localStorage.clear();
+
+let elemento;
+
+elemento = document;
+elemento = document.head;
+console.log(elemento);
+
+//vamos a seleccionar las imagenes de la pagina y convertirlas a un arreglo
+
+let imagenes = document.images;
+//podemos crear un Array desde una fuente de datos, siempre y cuando sean compatibles
+
+let imagenesArreglo = Array.from(imagenes);
+
+imagenesArreglo.forEach(function(imagen){
+    console.log(imagen);
+});
