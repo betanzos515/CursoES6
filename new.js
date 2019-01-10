bb9d8678bcd12cfd1130c17bc34497a10796ca77
betanzos515 <betanzos515@gmail.com>
2019-01-10 12:28:24 -0600

    Primero

---
+++ new.js
@@ -0,0 +1,10 @@
+let arreglo = ['Primero',12,'Segundo',false,'Tercero'];
+
+let objeto = {
+    primero:'David',
+    Segundo:'Edad',
+    edad:27
+};
+
+
+console.log(JSON.parse(localStorage.getItem('tweets')));
